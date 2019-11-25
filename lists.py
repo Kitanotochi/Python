c64e279f0eaf43552f0b32817471a46c0d1ca512
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2019-11-25 20:28:06 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -2,3 +2,7 @@
 operating_systems = ['Mac OS', 'Windows', 'Ubuntu', 'Fedora', 'Kali linux']
 print(operating_systems)
 
+# <len> - функция показывает колличество символов (длину строки)
+operating_systems_count = len(operating_systems)
+print(operating_systems_count)
+
