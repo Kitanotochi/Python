4cbe1634eef11ee996b0bcd09c4ad466c694cbc2
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2019-12-28 14:55:10 +0300

    Update tuple.py

--- Python_lessons/tuple.py
+++ Python_lessons/tuple.py
@@ -83,3 +83,17 @@ if name in user:
 else:
     print("Пользователь имеет другое имя")
 
+# Кортеж может содержать другие кортежи в виде элементов.
+# Пример:
+countries = (
+    ("Germany", 80.2, (("Berlin",3.326), ("Hamburg", 1.718))),
+    ("France", 66, (("Paris", 2.2),("Marsel", 1.6)))
+)
+ 
+for country in countries:
+    countryName, countryPopulation, cities = country
+    print("\nCountry: {}  population: {}".format(countryName, countryPopulation))
+    for city in cities:
+        cityName, cityPopulation = city
+        print("City: {}  population: {}".format(cityName, cityPopulation))
+
